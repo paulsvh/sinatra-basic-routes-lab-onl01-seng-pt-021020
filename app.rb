4b8b031aab35adbb8134a52867ffac4ea54f4d16
paul_vonhagen <pvonhagen26@gmail.com>
2020-04-30 18:52:28 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+    "My name is Paul"
+end
+
+get '/hometown' do
+    "My hometown is Seattle"
+end
+
+get '/favorite-song' do
+    "My favorite song is Touch The Sky by Kanye West"
+end
+
+
+
+
 end
